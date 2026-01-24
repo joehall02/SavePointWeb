d4538e6c01c25a430c03a881e2faf37ce770db36
joehall02 <93493198+joehall02@users.noreply.github.com>
2026-01-24 23:47:17 +0000

    Updated routing logic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
-import Router from "./router";
+import { Outlet } from "react-router-dom";
 import Nav from "../components/ui/Nav";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Nav />
-
-      <Router />
-    </BrowserRouter>
+      <Outlet />
+    </>
   );
 }
 
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import type { RouterType } from "../types/router.types";
 import pagesData from "./pagesData";
+import App from "../app/App";
 
-const Router = () => {
+export function AppRoutes() {
   const pageRoutes = pagesData.map(({ path, title, element }: RouterType) => {
     return <Route key={title} path={path} element={element} />;
   });
 
-  return <Routes>{pageRoutes}</Routes>;
+  return (
+    <Routes>
+      <Route element={<App />}>
+        {pageRoutes}
+      </Route>
+    </Routes>
+  )
 };
-
-export default Router;
