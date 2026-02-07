969c2ed46dcc28975e52a0b9a37bcdf99de78ccc
joehall02 <93493198+joehall02@users.noreply.github.com>
2026-02-07 13:56:21 +0000

    Tss-React
    - Setup test tss-react class to use on the home button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 import Button from "@mui/material/Button";
 import { HomeFilled } from "@mui/icons-material";
+import { useStyles } from "./styles";
 
 function Home() {
+	const { classes } = useStyles();
+	
 	return (
 		<div>
 			Home
-			<Button color="primary" variant="contained">
+			<Button color="primary" variant="contained" className={classes.testButton}>
 				<HomeFilled />
 				Click
 			</Button>
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,8 @@
+import type { Theme } from "@mui/material";
+import { makeStyles } from "tss-react/mui";
+
+export const useStyles = makeStyles()((theme: Theme) => ({
+	testButton: {
+		padding: theme.spacing(4)
+	}
+}));
