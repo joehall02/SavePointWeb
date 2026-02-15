546147ffdcafa85c4b5cf899ef09895dfa19643b
joehall02 <93493198+joehall02@users.noreply.github.com>
2026-02-15 20:05:40 +0000

    PlatformService
    - PlatformService class
    - getPlatforms function
    - Platform dao mapper helper/model types

---
+++ src/api/services/PlatformService.ts
@@ -0,0 +1,19 @@
+import { mapPlatformDaoToPlatform } from "../../helpers/daoMappers";
+import type { Platform } from "../../types/platform.types";
+import type { PlatformDao } from "../../types/platformDao.types";
+import SavePointApiManager from "../SavePointApiManager";
+
+class PlatformService {
+	private static platformsBaseUrl = '/api/platforms';
+
+	/**
+	 * @returns Normalised list of platforms
+	 */
+	public static async getPlatforms(): Promise<Platform[]> {
+		return SavePointApiManager
+			.get<PlatformDao[]>(`${this.platformsBaseUrl}`)
+			.then((response) => response.data.map(mapPlatformDaoToPlatform));
+	}
+}
+
+export default PlatformService
