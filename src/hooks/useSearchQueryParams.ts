415d148855a6ef3c2500e51a1acf38a5378dde5b
joehall02 <93493198+joehall02@users.noreply.github.com>
2026-04-12 00:56:42 +0100

    Moved URL Updating Logic Into Hook

---
+++ src/hooks/useSearchQueryParams.ts
@@ -0,0 +1,47 @@
+import { useCallback } from 'react';
+import type { SetURLSearchParams } from 'react-router-dom';
+
+/**
+ * Hook for handling query parameters from search, platform filtering, etc
+ * Returns a function that takes a key and an input value to update the query params.
+ * 
+ * @param setQueryParams - State updater for URL query params.
+ */
+export const useSearchQueryParams = (setQueryParams: SetURLSearchParams): (key: string, input: string) => void => (
+	useCallback((key: string, input: string): void => {
+		if (key === 'search' || key === 'platform') {
+			// Remove query param if it's empty
+			if (!input) {
+				setQueryParams(prev => (
+					((next => (next.delete(key), next))(new URLSearchParams(prev)))
+				));
+
+				return;
+			}
+
+			// If search is updated, clear all query params except search
+			// If platform is updated, clear all query params except platform & search
+			setQueryParams(prev => 
+				((next => (
+					next.set(key, input),
+					next.forEach((_value, paramKey) => 
+						key === 'platform' 
+							? ((paramKey !== 'search' && paramKey !=='platform') && next.delete(paramKey))
+							: (paramKey !== 'search' && next.delete(paramKey)),  
+					), next))(new URLSearchParams(prev))),
+			);
+			return;
+		}
+
+		// Update queryParams with updated value if input is not empty.
+		// If input is empty, remove the key from queryParams.
+		setQueryParams(prev => 
+			input 
+				? { ...Object.fromEntries(prev), [key]: input } 
+				// Immediately Invoked Function Expression (IIFE) instantly invokes
+				// the function using 'new URLSearchParams(prev)' as the argument.
+				// So here we define the function to delete the key, returning the new URLSearchParams object 'next'.
+				// Then we invoke it immediately with the previous queryParams as the argument.
+				: ((next => (next.delete(key), next))(new URLSearchParams(prev))));
+	}, [setQueryParams])
+);
--- src/pages/ExternalResults/index.tsx
+++ src/pages/ExternalResults/index.tsx
@@ -1,11 +1,12 @@
 import { Pagination } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import GameService from '../../api/services/GameService';
 import { Search } from '../../components/Search';
 import { fetchPagination } from '../../helpers/fetchPagination';
+import { useSearchQueryParams } from '../../hooks/useSearchQueryParams';
 import type { SearchGameResultsParams } from '../../types/game.types';
 import { useStyles } from './styles';
 
@@ -36,42 +37,7 @@ export const ExternalResults = () => {
 		queryFn: () => GameService.searchGameResults(searchParams),
 	});
 
-	const handleSearch = useCallback((key: string, input: string) => {
-		if (key === 'search' || key === 'platform') {
-			// Remove query param if it's empty
-			if (!input) {
-				setQueryParams(prev => (
-					((next => (next.delete(key), next))(new URLSearchParams(prev)))
-				));
-	
-				return;
-			}
-
-			// If search is updated, clear all query params except search
-			// If platform is updated, clear all query params except platform & search
-			setQueryParams(prev => 
-				((next => (
-					next.set(key, input),
-					next.forEach((_value, paramKey) => 
-						key === 'platform' 
-							? ((paramKey !== 'search' && paramKey !=='platform') && next.delete(paramKey))
-							: (paramKey !== 'search' && next.delete(paramKey)),  
-					), next))(new URLSearchParams(prev))),
-			);
-			return;
-		}
-
-		// Update queryParams with updated value if input is not empty.
-		// If input is empty, remove the key from queryParams.
-		setQueryParams(prev => 
-			input 
-				? { ...Object.fromEntries(prev), [key]: input } 
-				// Immediately Invoked Function Expression (IIFE) instantly invokes
-				// the function using 'new URLSearchParams(prev)' as the argument.
-				// So here we define the function to delete the key, returning the new URLSearchParams object 'next'.
-				// Then we invoke it immediately with the previous queryParams as the argument.
-				: ((next => (next.delete(key), next))(new URLSearchParams(prev))));
-	}, [setQueryParams]);
+	const handleSearch = useSearchQueryParams(setQueryParams);
 
 	return (
 		<div className={classes.root}>
